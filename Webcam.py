60bcdeed66ebc5649b49e8684e72969fd35bb465
GP1X3L <jeromem1013@gmail.com>
2026-06-18 20:02:49 -0400

    Update Webcam.py
    No OCR I added the wrong file

--- Webcam.py
+++ Webcam.py
@@ -1,16 +1,14 @@
 import cv2
 from PIL import Image
-import pytesseract
-import os
 
-cam = cv2.VideoCapture(0)  # Open the default webcam (device 0)
+cam = cv2.VideoCapture(0) 
 
-result, image = cam.read()  # Capture a single frame from the webcam and store success flag + image
-if result:  # Check if the image was successfully captured
+result, image = cam.read()  
+if result: 
   
-    cv2.imwrite("Picture-Text/photo.png", image)  # Save the captured image to a file
+    cv2.imwrite("Picture-Text/photo.png", image)  # Can turn this off later, Just for testing
 
-    img = Image.open("Picture-Text/photo.png")  # Open the saved image using PIL
+    img = Image.open("Picture-Text/photo.png") 
 
 
-cam.release()  
+cam.release() 
